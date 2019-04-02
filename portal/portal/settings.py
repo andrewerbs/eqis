364d01bfbed63d4d9bcdbf2f314fe26d0ba16e95
sandroroux <castellanese@gmail.com>
2019-04-02 16:10:46 +0900

    Imports local_settings. DATABASES is postgres.

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -91,8 +91,8 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myeqip_db',
     }
 }
 
@@ -161,3 +161,9 @@ WAGTAIL_SITE_NAME = "portal"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+# Import local_settings.
+try:
+    from .local_settings import *
+except ImportError:
+    pass
