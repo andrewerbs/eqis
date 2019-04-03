39861f27bb7921aedb90c306d207f4f435c77a64
sandroroux <castellanese@gmail.com>
2019-04-03 15:29:03 +0900

    Adds GA tracking to base.html.

--- portal/home/models.py
+++ portal/home/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 from wagtail.core.models import Page
 
+from portal.settings import GA_TAG
+
 
 class HomePage(Page):
-    pass
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        context.update({
+            'GA_TAG': GA_TAG,
+        })
+        return context
