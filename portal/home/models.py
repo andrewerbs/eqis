039efcd3b647747041cbabda406d305164640f9a
PeteCoward <peter@catalpa.io>
2019-04-05 11:39:48 +0900

    Update models.py

--- portal/home/models.py
+++ portal/home/models.py
@@ -8,7 +8,6 @@ from portal.settings import GA_TAG
 class HomePage(Page):
 
     def get_context(self, request):
-        # Update context to include only published posts, ordered by reverse-chron
         context = super().get_context(request)
         context.update({
             'GA_TAG': GA_TAG,
