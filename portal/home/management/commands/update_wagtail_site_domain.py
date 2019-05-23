9b0962929c5f54e51eb44c6f6a5e397813b0eef5
sandroroux <castellanese@gmail.com>
2019-05-23 16:54:53 +0900

    fixup

--- portal/home/management/commands/update_wagtail_site_domain.py
+++ portal/home/management/commands/update_wagtail_site_domain.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand, CommandError
 
 from wagtail.core.models import Site
 
+
 class Command(BaseCommand):
     help = 'Updates the Wagtail site with a user-provided domain.'
 
@@ -11,7 +12,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         new_domain = options['new_domain']
-        self.stdout.write(self.style.SUCCESS(new_domain))
         s = Site.objects.all()[0]
         s.hostname = new_domain
         s.save()
+        self.stdout.write(
+            f'Site is now at the domain, {self.style.SUCCESS(new_domain)}.'
+        )
